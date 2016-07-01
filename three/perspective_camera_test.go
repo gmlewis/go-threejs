125fcf834b1a805ef4550a9a0c4ca4593e0cd355
Glenn Lewis <gmlewis@google.com>
2016-06-30 21:28:31 -0700

    wip

---
+++ three/perspective_camera_test.go
@@ -0,0 +1,18 @@
+// Copyright 2016 Google Inc. All rights reserved.
+// Use of this source code is governed by the Apache 2.0
+// license that can be found in the LICENSE file.
+
+package three_test
+
+import (
+	"github.com/gmlewis/go-threejs/three"
+)
+
+func ExamplePerspectiveCamera() {
+	const width, height = 800, 600
+	t := three.New()
+	scene := t.Scene().New()
+
+	camera := t.PerspectiveCamera().New(45, width/height, 1, 1000)
+	scene.Add(camera)
+}
