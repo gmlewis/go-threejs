9c4d47fdd95a717c7da4c790f359b59e5ac3839b
Glenn Lewis <gmlewis@google.com>
2016-07-16 22:25:04 -0700

    Add another example

--- three/circle_buffer_geometry.go
+++ three/circle_buffer_geometry.go
@@ -8,8 +8,10 @@ import (
 	"github.com/gopherjs/gopherjs/js"
 )
 
-// CircleBufferGeometry represents a circlebuffergeometry.
-type CircleBufferGeometry struct{ p *js.Object }
+// CircleBufferGeometry is the BufferGeometry port of CircleGeometry.
+//
+// http://threejs.org/docs/index.html#Reference/Extras.Geometries/CircleBufferGeometry
+type CircleBufferGeometry struct{ *BufferGeometry }
 
 // JSObject returns the underlying *js.Object.
 func (c *CircleBufferGeometry) JSObject() *js.Object { return c.p }
@@ -22,11 +24,28 @@ func (t *Three) CircleBufferGeometry() *CircleBufferGeometry {
 
 // CircleBufferGeometryFromJSObject returns a wrapped CircleBufferGeometry JavaScript class.
 func CircleBufferGeometryFromJSObject(p *js.Object) *CircleBufferGeometry {
-	return &CircleBufferGeometry{p: p}
+	return &CircleBufferGeometry{BufferGeometryFromJSObject(p)}
+}
+
+// CircleBufferGeometryOpts represents options for NewCircleBufferGeometry.
+//
+//     thetaStart — Start angle for first segment, default = 0 (three o'clock position).
+//     thetaLength — The central angle, often called theta, of the circular sector. The default is 2*Pi, which makes for a complete circle.
+type CircleBufferGeometryOpts struct {
+	thetaStart  float64
+	thetaLength float64
 }
 
 // NewCircleBufferGeometry returns a new CircleBufferGeometry object.
-func (t *Three) NewCircleBufferGeometry(radius, segments, thetaStart, thetaLength float64) *CircleBufferGeometry {
-	p := t.ctx.Get("CircleBufferGeometry").New(radius, segments, thetaStart, thetaLength)
+//
+//     radius — Radius of the circle, default = 50.
+//     segments — Number of segments (triangles), minimum = 3, default = 8.
+func (t *Three) NewCircleBufferGeometry(radius, segments float64, opts *CircleBufferGeometryOpts) *CircleBufferGeometry {
+	var p *js.Object
+	if opts != nil {
+		p = t.ctx.Get("CircleBufferGeometry").New(radius, segments, opts.thetaStart, opts.thetaLength)
+	} else {
+		p = t.ctx.Get("CircleBufferGeometry").New(radius, segments)
+	}
 	return CircleBufferGeometryFromJSObject(p)
 }
