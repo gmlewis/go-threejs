a5a78b50b3bb83aee0bd9c3426650a1959191b79
Glenn Lewis <gmlewis@google.com>
2016-07-30 20:01:47 -0700

    Changes made by Susan

--- three/box3.go
+++ three/box3.go
@@ -26,25 +26,33 @@ func (t *Three) NewBox3(min, max float64) *Box3 {
 	return &Box3{p: p}
 }
 
-// Set TODO description.
+// Set sets the lower and upper (x, y) boundaries of this box.
+//
+//     min is lower (x, y) boundary of the box.
+//     max is upper (x, y) boundary of the box.
 func (b *Box3) Set(min, max float64) *Box3 {
 	b.p.Call("set", min, max)
 	return b
 }
 
-// SetFromArray TODO description.
+// SetFromArray TODO Description.
 func (b *Box3) SetFromArray(array float64) *Box3 {
 	b.p.Call("setFromArray", array)
 	return b
 }
 
-// SetFromPoints TODO description.
+// SetFromPoints sets the upper and lower bounds of this box to include all of the points in points.
+//
+//     points is the set of points that the resulting box will envelop.
 func (b *Box3) SetFromPoints(points float64) *Box3 {
 	b.p.Call("setFromPoints", points)
 	return b
 }
 
-// SetFromCenterAndSize TODO description.
+// SetFromCenterAndSize centers this box on center and sets this box's width and height to the values specified in size.
+//
+//     center is the desired center position of the box.
+//     size is the desired x and y dimensions of the box.
 func (b *Box3) SetFromCenterAndSize() *Box3 {
 	b.p.Call("setFromCenterAndSize")
 	return b
@@ -56,79 +64,100 @@ func (b *Box3) SetFromObject() *Box3 {
 	return b
 }
 
-// Clone TODO description.
+// Clone returns a copy of this box.
 func (b *Box3) Clone() *Box3 {
 	b.p.Call("clone")
 	return b
 }
 
-// Copy TODO description.
+// Copy copies the values of box to this box.
+//
+//     box is the box to copy.
 func (b *Box3) Copy(box *Box3) *Box3 {
 	b.p.Call("copy", box.p)
 	return b
 }
 
-// MakeEmpty TODO description.
+// MakeEmpty Makes this box empty.
 func (b *Box3) MakeEmpty() *Box3 {
 	b.p.Call("makeEmpty")
 	return b
 }
 
-// IsEmpty TODO description.
+// IsEmpty returns true if this box includes zero points within its bounds. Note that a box with equal lower and upper bounds still includes one point, the one both bounds share.
 func (b *Box3) IsEmpty() *Box3 {
 	b.p.Call("isEmpty")
 	return b
 }
 
-// Center TODO description.
+// Center returns the center point of this box.
+//
+//     optionalTarget, if specified, the result will be copied here.
 func (b *Box3) Center(optionalTarget float64) *Box3 {
 	b.p.Call("center", optionalTarget)
 	return b
 }
 
-// Size TODO description.
+// Size returns the width and height of this box.
+//
+//     optionalTarget, if specified, the result will be copied here.
 func (b *Box3) Size(optionalTarget float64) *Box3 {
 	b.p.Call("size", optionalTarget)
 	return b
 }
 
-// ExpandByPoint TODO description.
+// ExpandByPoint expands the boundaries of this box to include point.
+//
+//     point is the point that should be included in the box.
 func (b *Box3) ExpandByPoint(point float64) *Box3 {
 	b.p.Call("expandByPoint", point)
 	return b
 }
 
-// ExpandByVector TODO description.
+// ExpandByVector expands this box equilaterally by vector. The width of this box will be expanded by the x component of vector in both directions. The height of this box will be expanded by the y component of vector in both directions.
+//
+//     vector is the amount to expand this box in each dimension.
 func (b *Box3) ExpandByVector(vector float64) *Box3 {
 	b.p.Call("expandByVector", vector)
 	return b
 }
 
-// ExpandByScalar TODO description.
+// ExpandByScalar expands each dimension of the box by scalar. If negative, the dimensions of the box will be contracted.
+//
+//     scalar is the distance to expand.
 func (b *Box3) ExpandByScalar(scalar float64) *Box3 {
 	b.p.Call("expandByScalar", scalar)
 	return b
 }
 
-// ContainsPoint TODO description.
+// ContainsPoint returns true if the specified point lies within the boundaries of this box.
+//
+//    point is Vector2 to be checked for inclusion.
 func (b *Box3) ContainsPoint(point float64) *Box3 {
 	b.p.Call("containsPoint", point)
 	return b
 }
 
-// ContainsBox TODO description.
+// ContainsBox returns true if this box includes the entirety of box. If this and box overlap exactly, this function also returns true.
+//
+//     box to test for inclusion.
 func (b *Box3) ContainsBox(box float64) *Box3 {
 	b.p.Call("containsBox", box)
 	return b
 }
 
-// GetParameter TODO description.
+// GetParameter Returns a point as a proportion of this box's width and height.
+//
+//     point is Vector2
+//     optionalTarget is Vector2
 func (b *Box3) GetParameter(point, optionalTarget float64) *Box3 {
 	b.p.Call("getParameter", point, optionalTarget)
 	return b
 }
 
-// IntersectsBox TODO description.
+// IntersectsBox Determines whether or not this box intersects box.
+//
+//     box to check for intersection against.
 func (b *Box3) IntersectsBox(box float64) *Box3 {
 	b.p.Call("intersectsBox", box)
 	return b
@@ -140,13 +169,18 @@ func (b *Box3) IntersectsPlane(plane float64) *Box3 {
 	return b
 }
 
-// ClampPoint TODO description.
+// ClampPoint clamps point within the bounds of this box.
+//
+//     point is the position to clamp.
+//     optionalTarget, if specified, the clamped result will be copied here.
 func (b *Box3) ClampPoint(point, optionalTarget float64) *Box3 {
 	b.p.Call("clampPoint", point, optionalTarget)
 	return b
 }
 
-// DistanceToPoint TODO description.
+// DistanceToPoint returns the distance from any edge of this box to the specified point. If the point lies inside of this box, the distance will be 0.
+//
+//     point is the point to measure distance to.
 func (b *Box3) DistanceToPoint() *Box3 {
 	b.p.Call("distanceToPoint")
 	return b
@@ -158,13 +192,17 @@ func (b *Box3) GetBoundingSphere() *Box3 {
 	return b
 }
 
-// Intersect TODO description.
+// Intersect returns the intersection of this and box, setting the upper bound of this box to the lesser of the two boxes' upper bounds and the lower bound of this box to the greater of the two boxes' lower bounds.
+//
+//     box to intersect with.
 func (b *Box3) Intersect(box float64) *Box3 {
 	b.p.Call("intersect", box)
 	return b
 }
 
-// Union TODO description.
+// Union nions this box with box setting the upper bound of this box to the greater of the two boxes' upper bounds and the lower bound of this box to the lesser of the two boxes' lower bounds.
+//
+//     box that will be unioned with this box.
 func (b *Box3) Union(box float64) *Box3 {
 	b.p.Call("union", box)
 	return b
@@ -176,13 +214,17 @@ func (b *Box3) ApplyMatrix4() *Box3 {
 	return b
 }
 
-// Translate TODO description.
+// Translate adds offset to both the upper and lower bounds of this box, effectively moving this box offfset units in 2D space.
+//
+//     offset is the direction and distance of offset.
 func (b *Box3) Translate(offset float64) *Box3 {
 	b.p.Call("translate", offset)
 	return b
 }
 
-// Equals TODO description.
+// Equals returns true if this box and box share the same lower and upper bounds.
+//
+//     box to compare.
 func (b *Box3) Equals(box float64) *Box3 {
 	b.p.Call("equals", box)
 	return b
