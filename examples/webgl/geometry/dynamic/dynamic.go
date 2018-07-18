a293badfb5e5f9aa58add862e927a84fdfd3b0eb
Glenn Lewis <6598971+gmlewis@users.noreply.github.com>
2018-07-17 20:34:57 -0400

    Mark dynamic.go example as incomplete.

--- examples/webgl/geometry/dynamic/dynamic.go
+++ examples/webgl/geometry/dynamic/dynamic.go
@@ -12,6 +12,8 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+// NOTE: This example has not been completed yet.
+
 // See: http://threejs.org/examples/#webgl_geometry_dynamic
 package main
 
